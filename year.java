b213ca27e694bdcc022b8881ef6da7f33cbfaa10
naveena31 <naveena22896@gmail.com>
2017-06-09 16:59:53 +0530

    Create year.java

---
+++ year.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class year
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		if(n%4==0)
+		System.out.println("it is leap year");
+		else
+		System.out.println("it is not a leap year");
+	}
+}
+	
